8862fdff27fd7e5a857373387c8355fbf1b3f9ff
EMMANUEL-COLOMBANO <linobano7@gmail.com>
2018-01-24 12:37:29 +0300

    first cslculator

---
+++ calculation.js
@@ -0,0 +1,20 @@
+/*var button = document.getElementById("b1").addEventListener("click", function name() {
+    
+document.getElementById("button2").value;
+    var b = 1;
+document.getElementById("b1").innerHTML = b;
+    //console.log(b);
+    //  console.log(c);*/
+    
+   // });
+   // var button1 = document.getElementById("b2").addEventListener("click", function name() {
+        //var c =2;
+        //console.log(c);
+        
+      //  });
+      document.getElementById("b1").addEventListener("click", function(){
+var button = document.getElementById("b1").value;
+var b = 1;
+    console.log(b);
+//document.getElementById("b1").innerHTML = button;
+      });
